bc8d8edb33be902bdd0bd2d97078ad653a23aae3
Piotr Balbier <piotr.balbier@gmail.com>
2016-03-08 17:37:55 +0100

    Moze w koncu uda mi sie zgrac

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,5 @@ using namespace cv;
 using namespace std;
 
 int main() {
-        Mat OurImage;
-        string Destination = "Lena.jpg";
-        OurImage = imread(Destination, CV_LOAD_IMAGE_GRAYSCALE);
-        if (!OurImage.data) {
-                printf("No image!");
-                getchar();
-                return -1;
-        }
-        namedWindow("WINDOW", CV_WINDOW_AUTOSIZE);
-        imshow("WINDOW", OurImage);
-        waitKey(0);
+    printf("Empty main()\n");
 }
