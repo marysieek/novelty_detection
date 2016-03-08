307edb4259ef49ca2c9256766439aaac05f89947
Piotr Balbier <piotr.balbier@gmail.com>
2016-03-08 16:49:48 +0100

    Added support for OpenCV

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,21 @@
-int main() {}
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+        Mat OurImage;
+        string Destination = "Lena.jpg";
+        OurImage = imread(Destination, CV_LOAD_IMAGE_GRAYSCALE);
+        if (!OurImage.data) {
+                printf("No image!");
+                getchar();
+                return -1;
+        }
+        namedWindow("WINDOW", CV_WINDOW_AUTOSIZE);
+        imshow("WINDOW", OurImage);
+        waitKey(0);
+}
