520d9983324929a53dcc404fc16443e405434ccc
Maria Stoklosa (marysieek) <stoklosama@gmail.com>
2016-05-21 12:33:55 +0200

    Add matching by area of holes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
 #include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -40,12 +41,17 @@ int main( int argc, char** argv )
 
     Mat3b imageBgr, hsv;
     Mat1b mask1, mask2;
+    Mat gray;
 
     vector<int> amountOfPixels;
+    vector<int> areaOfHoles;
+    vector<int> amountOfHoles;
     vector<Point> nonBlackPixels;
 
 
-    for(int i=0; i < fileList.size(); i++){
+    // Implement filtering of images by amount of red colour in the photo
+
+    for(int i = 0; i < fileList.size(); i++){
         Mat readImage;
         string separator = "/";
         string path = argv[1] + separator + fileList[i];
@@ -58,26 +64,61 @@ int main( int argc, char** argv )
 
         findNonZero(mask, nonBlackPixels);
         amountOfPixels.push_back(nonBlackPixels.size());
-        cout << nonBlackPixels.size() << endl;
-
-        //namedWindow( "window", WINDOW_AUTOSIZE );// Create a window for display.
-        //imshow("window", mask);
-        //waitKey(1000);
     }
 
 
     int smallestArea = *min_element(amountOfPixels.begin(), amountOfPixels.end());
     int largestArea = *max_element(amountOfPixels.begin(), amountOfPixels.end());
+
+    // Implement filtering of images by area of contours
+
+    for(int i = 0; i < fileList.size(); i++) {
+        Mat readImage;
+        string separator = "/";
+        string path = argv[1] + separator + fileList[i];
+
+        imageBgr = imread(path);
+
+        GaussianBlur( imageBgr, imageBgr, Size( 3, 3 ), 0, 0 );
+        cvtColor(imageBgr, gray, cv::COLOR_BGR2GRAY);
+
+        erode(gray, gray, Mat(), Point(-1, -1), 2, 1, 1);
+        dilate(gray, gray, Mat(), Point(-1, -1), 2, 1, 1);
+        cv::Canny(gray, gray, 200, 20);
+
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+
+        findContours( gray, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+         int contoursAmount = 0;
+         areaOfHoles.clear();
+
+         for(unsigned int i=0;i<contours.size();i++) {
+            areaOfHoles.push_back(contourArea(contours[i]));
+         }
+
+        cout << areaOfHoles.size() << endl;
+        amountOfHoles.push_back(areaOfHoles.size());
+
+    }
+
+    int smallestNumOfHoles = *min_element(amountOfHoles.begin(), amountOfHoles.end());
+    int largestNumOfHoles = *max_element(amountOfHoles.begin(), amountOfHoles.end());
+
     int matchingPhotosCount = 0;
 
-    for(int i = 0; i < amountOfPixels.size(); i++) {
-        if ((amountOfPixels[i] >= 85) && (amountOfPixels[i] <= 3562)) {
+    for(int i = 0; i < amountOfHoles.size(); i++) {
+        if ((amountOfHoles[i] >= 9) && (amountOfHoles[i] <= 306) && (amountOfPixels[i] >= 85) && (amountOfPixels[i] <= 3562)) {
             matchingPhotosCount++;
         }
     }
 
+
     cout << "Smallest area: " << smallestArea << endl;
     cout << "Largest area: " << largestArea << endl;
+    cout << "Smallest number of holes: " << smallestNumOfHoles << endl;
+    cout << "Largest number of holes: " << largestNumOfHoles << endl;
     cout << "Photos matching: " << matchingPhotosCount << "/" << fileList.size() << endl;
 
     return 0;
